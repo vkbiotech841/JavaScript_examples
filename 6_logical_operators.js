9c918a64325d4d3bf12ff21188800de05329795d
VIKRAM KUMAR <vkbiotech841@gmail.com>
2021-11-05 13:24:28 +0530

    refinements

--- 6_logical_operators.js
+++ 6_logical_operators.js
@@ -1,22 +1,22 @@
 // Logical And (&&)operator
 
-console.log("true && true:", true && true);
-console.log("true && false:", true && false);
-console.log("false && true:", false && true);
+console.log("true && true:", true && true);       //ans: true
+console.log("true && false:", true && false);     //ans: false
+console.log("false && true:", false && true);     //ans: false
 
 // Logical OR (||) operator
 
-console.log("true || true:", true || true);
-console.log("true || false:", true || false);
-console.log("false || true:", false || true);
+console.log("true || true:", true || true);       //ans:true
+console.log("true || false:", true || false);     //ans: true
+console.log("false || true:", false || true);     //ans: true
 
 // Tricky 
-console.log("true && 'Hi':", true && "Hi");
-console.log("true && 'Hi' && 1:", true && "Hi" && 1);
-console.log("false && 'Hi':", false && "Hi");
-console.log("false || 'Hi':", false || "Hi");
-console.log("'Hi' && false:", "Hi" && false);
-console.log("'Hi' || false:", "Hi" || false);
+console.log("true && 'Hi':", true && "Hi");                 //ans:'Hi'
+console.log("true && 'Hi' && 1:", true && "Hi" && 1);       //ans:1
+console.log("false && 'Hi':", false && "Hi");               //ans:false
+console.log("'Hi' && false:", "Hi" && false);               //ans: false
+console.log("false || 'Hi':", false || "Hi");               //ans: 'Hi'
+console.log("'Hi' || false:", "Hi" || false);               //ans: 'Hi'
 
 
 // Example of && operator
